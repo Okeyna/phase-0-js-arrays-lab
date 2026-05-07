eefaacfcb4858a95385aa2d4032704d2809fe8cd
Okechukwu Uneze <unezeo@gmail.com>
2026-05-07 14:47:29 -0400

    finished lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,21 @@
 // Write your code here
+const products = ['Laptop', 'Phone', 'Headphones', 'Monitor'];
 
+const logFirstProduct = () => {
+  console.log(products[0]);
+}
 
+const addProduct = (productName) => {
+  products.push(productName);
+}
+
+const updateProductName = (index, newName) => {
+  products[index] = newName;
+}
+
+const removeLastProduct = () => {
+  products.pop();
+}
 
 // Export the necessary parts for testing
 module.exports = {
